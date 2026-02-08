ea039c3e98ac0d1f95938b7573e81fa35e2bd23b
Hugo Fragnoli <hugofragnoli@gmail.com>
2026-02-08 15:38:29 +0100

    gestion erreur filterstring.py

--- ex06/filterstring.py
+++ ex06/filterstring.py
@@ -5,3 +5,10 @@ from ft_filter import ft_filter
 def main():
 
     nb_args = len(sys.argv) - 1
+    try:
+        if nb_args != 2:
+            raise AssertionError("the arguments are bad")
+        deuxieme_arg = int(sys.argv[2])
+
+    except ValueError:
+        raise AssertionError("the arguments are bad")
