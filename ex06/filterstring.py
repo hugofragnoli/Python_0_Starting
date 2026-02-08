95edea0e0ebea599202dadbf1d9f3176d748809a
Hugo Fragnoli <hugofragnoli@gmail.com>
2026-02-08 16:05:54 +0100

    filterstring + explications lambda

--- ex06/filterstring.py
+++ ex06/filterstring.py
@@ -3,12 +3,47 @@ from ft_filter import ft_filter
 
 
 def main():
-
-    nb_args = len(sys.argv) - 1
+    """
+    Filters words from string S with length strictly greater than N.
+    """
     try:
-        if nb_args != 2:
+        if len(sys.argv) != 3:
             raise AssertionError("the arguments are bad")
-        deuxieme_arg = int(sys.argv[2])
+        
+        try:
+            n_min = int(sys.argv[2])
+        except ValueError:
+            raise AssertionError("the arguments are bad")
 
-    except ValueError:
-        raise AssertionError("the arguments are bad")
+        s_arg = sys.argv[1]
+        
+        #Ici on crée une fonction invisible.
+        # lambda word = ecrire def function(word)
+        # On definit que notre func prend un arg "word"
+        # len(word) > n_min -> cest le test :
+        # La focntion calcule la len du mot et compare a n_min
+        # Une lambda renvoie true ou false en fonction du res.
+        # Si len > n_min -> true
+        check_len = lambda word: len(word) > n_min
+
+        # List comprehension contrainte
+        # .split() pour isoler les mots
+        #s.arg.split(decoupe arg 1 en une liste de mots indiv)
+        # for w in : -> boucle-> on prend chaque mot de la list et on le soumet a filter
+        # chaque mot sappelle w temporairement.
+        #ft_filter  parcourt chaque mot et demande a lambda si cest true ou false.
+        # si true, ft_filter garde le mot dans son iterateur.
+        result = [w for w in ft_filter(check_len, s_arg.split())]
+        #le result est un type list
+        # pour montrer si on me demande
+        # type_result = type(result)
+        # print(f"type de result est {type_result}")
+
+        print(result)
+
+    except AssertionError as e:
+        print(f"AssertionError: {e}")
+
+
+if __name__ == "__main__":
+    main()
